e6e077c6aa645d3b887d5e7407e49c11551138dc
Mac17369 <Mac17369@uvg.edu.gt>
2020-11-27 09:38:23 -0600

    moar coding

--- datos.py
+++ datos.py
@@ -20,8 +20,8 @@ def ventanaDatos():
     label1.pack()
    
 
-    label4 = tk.Label(ventanaDatos, text="", font=("Courier", 17), bg="LightGreen", fg="black")
-    label4.pack(side=tk.TOP)
+    trash = tk.Label(ventanaDatos, text="", font=("Courier", 17), bg="LightGreen", fg="black")
+    trash.pack(side=tk.TOP)
 
     campo2 = tk.Frame(ventanaDatos, bg = "LightGreen")
     label2 = tk.Label(campo2, text="Particula creada:", font=("Courier", 17), bg="LightGreen", fg="black")
@@ -51,13 +51,35 @@ def ventanaDatos():
     label4 = tk.Label(ventanaDatos, text="", font=("Courier", 17), bg="LightGreen", fg="black")
     label4.pack(side=tk.TOP)
 
+#
+    trash = tk.Label(ventanaDatos, text="", font=("Courier", 17), bg="LightGreen", fg="black")
+    trash.pack(side=tk.TOP)
 
+    campo2 = tk.Frame(ventanaDatos, bg = "LightGreen")
+    label2 = tk.Label(campo2, text="Datos de escenario:", font=("Courier", 17), bg="LightGreen", fg="black")
+    label2.pack(side=tk.TOP)
+    
+    label2 = tk.Label(campo2, text="Voltaje:", font=("Courier", 13), bg="LightGreen", fg="black")
+    label2.pack(side=tk.LEFT, ipady = 7)
+    
+    usuario = tk.Text(campo2, width=12, height=1)
+    usuario.pack(side = tk.LEFT)
+
+    campo2.pack(side=tk.TOP)
+#
+
+    trash = tk.Label(ventanaDatos, text="", font=("Courier", 17), bg="LightGreen", fg="black")
+    trash.pack(side=tk.TOP)
     campo3 = tk.Frame(ventanaDatos, bg = "LightGreen")
 
     label4 = tk.Label(campo3, text="Seleccione particulas:", font=("Courier", 17), bg="LightGreen", fg="black")
     label4.pack(side=tk.TOP)
 
+    campo3.pack(side=tk.TOP)
+
+    trash = tk.Label(ventanaDatos, text="", font=("Courier", 17), bg="LightGreen", fg="black")
+    trash.pack(side=tk.TOP)
+
     button1 = tk.Button(ventanaDatos, text = "Simulacion", font=("Courier", 15))
     button1.pack(pady=10, ipadx=5)
 
-ventanaDatos()
